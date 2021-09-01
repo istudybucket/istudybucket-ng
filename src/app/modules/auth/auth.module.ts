5c51ca2e159500c40e81c8605cc1aed8d0573274
Elroy Kanye <elroykanye@gmail.com>
2021-09-02 00:26:37 +0100

    ek-autocomm: added ./auth.module.ts

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RegisterationTitleComponent } from './registeration-title/registeration-title.component';
+import { RegisterationFormComponent } from './registeration-form/registeration-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { RegisterTitleComponent } from './register-title/register-title.component';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    RegisterationTitleComponent,
+    RegisterationFormComponent,
+    RegisterFormComponent,
+    RegisterTitleComponent
   ],
   imports: [
     CommonModule
