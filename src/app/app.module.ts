c20b774fe4cfd92a857e536762d811582469855b
Elroy Kanye <elroykanye@gmail.com>
2021-09-01 20:05:19 +0100

    started up auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Feature Modules
+import { AuthModule } from "./modules/auth/auth.module";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
