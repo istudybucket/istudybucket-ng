981eb50f9830454f6e4c7549739e8165bb1531eb
Elroy Kanye <elroykanye@gmail.com>
2021-09-02 16:25:49 +0100

    ek-autocomm: added ./hero-landing/hero-landing.component.ts

---
+++ src/app/modules/landing/hero-landing/hero-landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-landing',
+  templateUrl: './hero-landing.component.html',
+  styleUrls: ['./hero-landing.component.css']
+})
+export class HeroLandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
