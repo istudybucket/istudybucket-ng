5468dca4099ff73ed120a6ea7d57ac78a3e665a5
Elroy Kanye <elroykanye@gmail.com>
2021-09-02 16:25:48 +0100

    ek-autocomm: added moasndon

---
+++ src/app/modules/landing/nav-landing/nav-landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-landing',
+  templateUrl: './nav-landing.component.html',
+  styleUrls: ['./nav-landing.component.css']
+})
+export class NavLandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
