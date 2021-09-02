23ca1db97164acdc6665944982ea69507b4e8795
Elroy Kanye <elroykanye@gmail.com>
2021-09-02 16:25:49 +0100

    ek-autocomm: added ./fake-landing/fake-landing.component.ts

---
+++ src/app/modules/landing/fake-landing/fake-landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fake-landing',
+  templateUrl: './fake-landing.component.html',
+  styleUrls: ['./fake-landing.component.css']
+})
+export class FakeLandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
