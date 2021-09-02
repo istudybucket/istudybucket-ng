afd408728af2cf2a9b30dd140ff67f55cfa3993e
Elroy Kanye <elroykanye@gmail.com>
2021-09-02 16:25:49 +0100

    ek-autocomm: added ./landing.module.ts

--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainLandingComponent } from './main-landing/main-landing.component';
+import { NavLandingComponent } from './nav-landing/nav-landing.component';
+import { HeroLandingComponent } from './hero-landing/hero-landing.component';
+import { FakeLandingComponent } from './fake-landing/fake-landing.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainLandingComponent,
+    NavLandingComponent,
+    HeroLandingComponent,
+    FakeLandingComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MainLandingComponent,
+    NavLandingComponent,
+    HeroLandingComponent,
+    FakeLandingComponent
   ]
 })
 export class LandingModule { }
