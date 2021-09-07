840162804c06400d3f93765bab4d44b25a25ce64
Elroy Kanye <elroykanye@gmail.com>
2021-09-07 16:18:02 +0100

    ek-autocomm: added ./app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {AuthModule} from "./modules/auth/auth.module";
+import {NavModule} from "./modules/nav/nav.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule,
+        AuthModule,
+        NavModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
