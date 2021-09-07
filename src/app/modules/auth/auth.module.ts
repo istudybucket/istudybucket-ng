6c0759cafddee75a068db9571aa4ba22fbc372ef
Elroy Kanye <elroykanye@gmail.com>
2021-09-07 16:18:01 +0100

    ek-autocomm: added ./app/modules/auth/auth.module.ts

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {ReactiveFormsModule} from "@angular/forms";
+import { FormTitleComponent } from './form-title/form-title.component';
+import { AuthComponent } from './auth/auth.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginFormComponent,
+    RegisterFormComponent,
+    FormTitleComponent,
+    AuthComponent
+  ],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    LoginFormComponent,
+    RegisterFormComponent,
+    FormTitleComponent,
+    AuthComponent
+  ]
+})
+export class AuthModule { }
