6d0f1a4e01633046c8a9bce7a784281020c31a26
Elroy Kanye <elroykanye@gmail.com>
2021-09-10 00:51:20 +0100

    ek-autocomm: added ./home/home-feed/home-feed.component.spec.ts

---
+++ src/app/modules/home/home-feed/home-feed.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeFeedComponent } from './home-feed.component';
+
+describe('HomeFeedComponent', () => {
+  let component: HomeFeedComponent;
+  let fixture: ComponentFixture<HomeFeedComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HomeFeedComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeFeedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
