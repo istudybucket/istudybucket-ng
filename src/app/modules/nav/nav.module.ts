33c2f0476ac32eb2c758bb0aa4eb96f27ae1e843
elroykanye <elroykanye@gmail.com>
2021-09-20 23:28:12 +0100

    update to app theme: modified ./src/app/modules/nav/nav.module.ts

--- src/app/modules/nav/nav.module.ts
+++ src/app/modules/nav/nav.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import {AuthModule} from "../auth/auth.module";
+import { MainNavComponent } from './main-nav/main-nav.component';
 import { NavLogoComponent } from './nav-logo/nav-logo.component';
 import { NavAreaComponent } from './nav-area/nav-area.component';
+import {SharedModule} from "../shared.module";
 
 
 
@@ -20,12 +15,7 @@ import { NavAreaComponent } from './nav-area/nav-area.component';
     ],
   imports: [
     CommonModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
+    SharedModule,
     AuthModule
   ]
 })
