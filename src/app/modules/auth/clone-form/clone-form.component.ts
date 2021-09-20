faa1b3a19d9809bf902e9d9d1a03bf8821671aa0
elroykanye <elroykanye@gmail.com>
2021-09-20 23:28:13 +0100

    update to app theme: modified ./src/app/modules/auth/clone-form/clone-form.component.ts

---
+++ src/app/modules/auth/clone-form/clone-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clone-form',
+  templateUrl: './clone-form.component.html',
+  styleUrls: ['./clone-form.component.css']
+})
+export class CloneFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
