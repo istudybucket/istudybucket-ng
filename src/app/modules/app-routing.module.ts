a608d8cbfb246c90046a11bd62aa7cace95392cc
elroykanye <elroykanye@gmail.com>
2021-09-21 01:18:41 +0100

    init the profile component - fixed routes: modified moasndon

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
-import {IsbGuardGuard} from "../../guards/isb-guard.guard";
-import {AuthComponent} from "../auth/auth/auth.component";
-import {ChatComponent} from "../chat/chat/chat.component";
-import {HomeComponent} from "../home/home/home.component";
+import {IsbGuardGuard} from "../guards/isb-guard.guard";
+import {AuthComponent} from "./auth/auth/auth.component";
+import {ChatComponent} from "./chat/chat/chat.component";
+import {HomeComponent} from "./home/home/home.component";
 
 const routes : Routes = [
   {
