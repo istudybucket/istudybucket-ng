cae7507a0304d1a718ca76b2a9443dc013c164e8
elroykanye <elroykanye@gmail.com>
2021-09-21 01:18:43 +0100

    init the profile component - fixed routes: modified ./src/app/modules/nav/nav.module.ts

--- src/app/modules/nav/nav.module.ts
+++ src/app/modules/nav/nav.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AuthModule} from "../auth/auth.module";
 import { MainNavComponent } from './main-nav/main-nav.component';
-import { NavLogoComponent } from './nav-logo/nav-logo.component';
-import { NavAreaComponent } from './nav-area/nav-area.component';
 import {SharedModule} from "../shared.module";
 
 
 
 @NgModule({
-    declarations: [MainNavComponent, NavLogoComponent, NavAreaComponent],
+    declarations: [MainNavComponent],
     exports: [
         MainNavComponent
     ],
