8acf547c2b9c0397cefe2126a8f6d1fb0ba6d74b
elroykanye <elroykanye@gmail.com>
2021-09-21 01:18:43 +0100

    init the profile component - fixed routes: modified ./src/app/modules/nav/main-nav/main-nav.component.ts

--- src/app/modules/nav/main-nav/main-nav.component.ts
+++ src/app/modules/nav/main-nav/main-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuItem} from "../../../models/interfaces/menu-item";
 
 @Component({
   selector: 'app-main-nav',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavComponent implements OnInit {
 
-  constructor() { }
+  menuItems: MenuItem[];
+  title: string;
+
+  constructor() {
+    this.title = "iSB";
+
+    this.menuItems = [
+      {label: 'Home', icon: 'home', badgeValue: 0, routerLink: '/home'},
+      {label: 'Friends', icon: 'people', badgeValue: 3, routerLink: '/chat'},
+      {label: 'Chat', icon: 'chat', badgeValue: 5, routerLink: '/home'},
+      {label: 'Notifications', icon: 'notifications', badgeValue: 34, routerLink: '/chat'},
+      {label: 'Buckets', icon: 'groups', badgeValue: 9, routerLink: '/auth'},
+    ];
+  }
 
   ngOnInit(): void {
   }
 
+
+  isBadgeHidden(badgeValue: number) : boolean{
+    return badgeValue <= 0;
+  }
 }
