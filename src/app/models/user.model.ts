48fb51b6bbdea617076bc86ed5d1e276b7678439
elroykanye <elroykanye@gmail.com>
2021-09-21 01:18:46 +0100

    init the profile component - fixed routes: modified ./src/app/models/user.model.ts

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,10 +1,22 @@
 export class User {
   username: string;
   email: string;
+  phoneNumber: string;
+  firstName: string;
+  lastName: string;
+  dob: string;
+  gender: string;
+  userId: number;
 
-  constructor(username: string, email: string) {
+
+  constructor(username: string, email: string, phoneNumber: string, firstName: string, lastName: string, dob: string, gender: string, userId: number) {
     this.username = username;
     this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = dob;
+    this.gender = gender;
+    this.userId = userId;
   }
-
 }
