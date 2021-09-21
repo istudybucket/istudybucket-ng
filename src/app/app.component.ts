eca1d14a808756aa29c1ffec76195c268285e22e
elroykanye <elroykanye@gmail.com>
2021-09-21 01:18:42 +0100

    init the profile component - fixed routes: modified ./src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'istudybucket-ngk';
+
+  constructor() {
+    //this.activateNavEffect();
+  }
+
+  activateNavEffect() {
+    window.onscroll = function () {
+      stickNavigation();
+    };
+
+    let isbNavbar = document.getElementById("isb-navbar");
+    let sticky = 0;
+
+    if(isbNavbar != null) {
+      sticky = isbNavbar.offsetTop;
+    }
+    const stickNavigation = () => {
+      if(window.pageYOffset >= sticky && isbNavbar != null) {
+        isbNavbar.classList.add("sticky");
+      }
+    }
+  }
 }
